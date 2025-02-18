3b6cec87f19194b302b3ba958549ec7d72dee1f1
Buha-hepin <buhahepin.v@gmail.com>
2025-02-18 22:02:08 +0530

    abc

---
+++ js/app.js
@@ -0,0 +1,189 @@
+
+// ------------------------------------------------
+// Table of Contents
+// ------------------------------------------------
+//
+//  01. Loader & Loading Animation
+//  02. Bootstrap Scroll Spy Plugin Settings
+//  03. Lenis Scroll Plugin
+//  04. Parallax
+//  05. Scroll Animations
+//  06. Smooth Scrolling
+//  07. Swiper Slider
+//  08. Contact Form
+//  09. Modernizr SVG Fallback
+//  10. Chrome Smooth Scroll
+//  11. Images Moving Ban
+//  12. Detecting Mobile/Desktop
+//  13. PhotoSwipe Gallery Images Replace
+//  14. Color Switch
+//
+// ------------------------------------------------
+// Table of Contents End
+// ------------------------------------------------
+
+$(function() {
+
+  "use strict";
+
+  gsap.registerPlugin(ScrollTrigger);
+  // -------------------------------------------------------------------------------------------------------//
+  // Lenis Scroll Plugin Start
+  const lenis = new Lenis()
+  function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+  requestAnimationFrame(raf)
+  // Lenis Scroll Plugin End
+  // ------------------------------------------------------------------------------------------------------- //
+
+
+  // --------------------------------------------- //
+  // Scroll Animations Start
+  // Animation In Up
+  const animateInUp = document.querySelectorAll(".animate-in-up");
+  animateInUp.forEach((element) => {
+    gsap.fromTo(element, {
+      opacity: 0,
+      y: 50,
+      ease: 'sine',
+    }, {
+      y: 0,
+      opacity: 1,
+      scrollTrigger: {
+        trigger: element,
+        toggleActions: 'play none none reverse',
+      }
+    });
+  });
+
+  // Animation Rotation
+  const animateRotation = document.querySelectorAll(".animate-rotation");
+  animateRotation.forEach((section) => {
+    var value = $(section).data("value");
+    gsap.fromTo(section, {
+      ease: 'sine',
+      rotate: 0,
+    }, {
+      rotate: value,
+      scrollTrigger: {
+        trigger: section,
+        scrub: true,
+        toggleActions: 'play none none reverse',
+      }
+    });
+  });
+
+  // Animation Cards Stack
+  // Grid 2x
+  gsap.set(".animate-card-2", {y: 100, opacity: 0});
+  ScrollTrigger.batch(".animate-card-2", {
+    interval: 0.1,
+    batchMax: 2,
+    duration: 6,
+    onEnter: batch => gsap.to(batch, {
+      opacity: 1, 
+      y: 0,
+      ease: 'sine',
+      stagger: {each: 0.15, grid: [1, 2]}, 
+      overwrite: true
+    }),
+    onLeave: batch => gsap.set(batch, {opacity: 1, y: 0, overwrite: true}),
+    onEnterBack: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: 0.15, overwrite: true}),
+    onLeaveBack: batch => gsap.set(batch, {opacity: 0, y: 100, overwrite: true})
+  });
+
+  // Grid 3x
+  gsap.set(".animate-card-3", {y: 50, opacity: 0});
+  ScrollTrigger.batch(".animate-card-3", {
+    interval: 0.1,
+    batchMax: 3,
+    duration: 3,
+    onEnter: batch => gsap.to(batch, {
+      opacity: 1, 
+      y: 0,
+      ease: 'sine',
+      stagger: {each: 0.15, grid: [1, 3]}, 
+      overwrite: true
+    }),
+    onLeave: batch => gsap.set(batch, {opacity: 1, y: 0, overwrite: true}),
+    onEnterBack: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: 0.15, overwrite: true}),
+    onLeaveBack: batch => gsap.set(batch, {opacity: 0, y: 50, overwrite: true})
+  });
+
+  // Grid 5x
+  gsap.set(".animate-card-5", {y: 50, opacity: 0});
+  ScrollTrigger.batch(".animate-card-5", {
+    interval: 0.1,
+    batchMax: 5,
+    delay: 1000,
+    onEnter: batch => gsap.to(batch, {
+      opacity: 1, 
+      y: 0,
+      ease: 'sine',
+      stagger: {each: 0.15, grid: [1, 5]}, 
+      overwrite: true
+    }),
+    onLeave: batch => gsap.set(batch, {opacity: 1, y: 0, overwrite: true}),
+    onEnterBack: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: 0.15, overwrite: true}),
+    onLeaveBack: batch => gsap.set(batch, {opacity: 0, y: 50, overwrite: true})
+  });
+
+  ScrollTrigger.addEventListener("refreshInit", () => gsap.set(".animate-card-2", {y: 0, opacity: 1}));
+  ScrollTrigger.addEventListener("refreshInit", () => gsap.set(".animate-card-3", {y: 0, opacity: 1}));
+  ScrollTrigger.addEventListener("refreshInit", () => gsap.set(".animate-card-5", {y: 0, opacity: 1}));
+  // --------------------------------------------- //
+  // Scroll Animations End
+  // --------------------------------------------- //
+
+
+  
+
+  // --------------------------------------------- //
+  // Contact Form Start
+  // --------------------------------------------- //
+  $("#contact-form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+      $('.contact').find('.form').addClass('is-hidden');
+      $('.contact').find('.form__reply').addClass('is-visible');
+			setTimeout(function() {
+				// Done Functions
+        $('.contact').find('.form__reply').removeClass('is-visible');
+        $('.contact').find('.form').delay(300).removeClass('is-hidden');
+				th.trigger("reset");
+			}, 5000);
+		});
+		return false;
+	});
+  // --------------------------------------------- //
+  // Contact Form End
+  // --------------------------------------------- //
+
+
+
+
+  //remove this and add project box.......
+  // --------------------------------------------- //
+  // PhotoSwipe Gallery Images Replace Start
+  // --------------------------------------------- //
+  $('.gallery__link').each(function(){
+    $(this)
+    .append('<div class="picture"></div>')
+    .children('.picture').css({'background-image': 'url('+ $(this).attr('data-image') +')'});
+  });
+  // --------------------------------------------- //
+  // PhotoSwipe Gallery Images Replace End
+  // --------------------------------------------- //
+
+});
+
+
+
+
+
